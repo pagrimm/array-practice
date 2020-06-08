90f6bd6ef38b45c5a941a265f4372ad9f8f8203e
Peter Grimm <pagrimm@gmail.com>
2020-06-08 10:20:51 -0700

    add logic to display results
    Co-authored-by: Thomas Glenn <thomasgrantglenn@mac.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,17 @@ $(document).ready(function () {
     event.preventDefault();
     let favArray = [];
     favArray.push($("input#fav-color").val());
+    favArray.push($("input#fav-animal").val());
     favArray.push($("input#fav-food").val());
     favArray.push($("input#fav-month").val());
     favArray.push($("input#fav-movie").val());
     favArray.push($("input#fav-icecream").val());
+    let newArray = [];
+    newArray.push(favArray[1]);
+    newArray.push(favArray[0]);
+    newArray.push(favArray[2]);
+    $("#results-list").append("<li>" + newArray[0] + "</li>");
+    $("#results-list").append("<li>" + newArray[1] + "</li>");
+    $("#results-list").append("<li>" + newArray[2] + "</li>");
   });
 });
